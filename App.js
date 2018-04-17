5985a9b2098870e349f85fe201ed1ce45af197eb
Bill Buchanan <bbuchanan@gmail.com>
2018-04-17 16:42:46 -0400

    added scores and error to state

--- App.js
+++ App.js
@@ -4,18 +4,38 @@ import axios from "./src/axios-sports";
 import ScorecardItem from "./src/components/ScorecardItem";
 
 export default class App extends React.Component {
+  state = {
+    scores: null,
+    hadError: false,
+    errorMessage: ""
+  };
+
   componentDidMount() {
-    axios.get("scoreboard.json?fordate=20170411").then(data => {
-      let scores = data.data.scoreboard.gameScore.map(score => {
-        return {
-          awayScore: score.awayScore,
-          homeScore: score.homeScore,
-          homeTeam: `${score.game.homeTeam.City} ${score.game.homeTeam.Name}`,
-          awayTeam: `${score.game.awayTeam.City} ${score.game.awayTeam.Name}`,
-          location: score.game.location
-        };
+    axios
+      .get("scoreboard.json?fordate=20170411")
+      .then(data => {
+        let scores = data.data.scoreboard.gameScore.map(score => {
+          return {
+            awayScore: score.awayScore,
+            homeScore: score.homeScore,
+            homeTeam: `${score.game.homeTeam.City} ${score.game.homeTeam.Name}`,
+            awayTeam: `${score.game.awayTeam.City} ${score.game.awayTeam.Name}`,
+            location: score.game.location
+          };
+        });
+        this.setState({
+          scores,
+          hadError: false,
+          errorMessage: ""
+        });
+      })
+      .catch(err => {
+        this.setState({
+          scores: null,
+          hadError: true,
+          errorMessage: err.message
+        });
       });
-    });
   }
 
   render() {
